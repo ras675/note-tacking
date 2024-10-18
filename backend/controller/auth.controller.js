6470eefc32109b295a6d38d2fb7100ca44b36eab
rakins <shahriar.rakin99@gmail.com>
2024-10-19 05:49:20 +0600

    backend done

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next)=> {
     const {username, email, password} = req.body;
@@ -27,8 +28,51 @@ export const signup = async (req, res,next)=> {
             success: true,
             message: 'User created successfully'
     })
-} catch (error) {
+}   catch (error) {
     next(error);
 
 }
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({email});
+
+        if(!validUser) {
+            return next(errorHandler(404, 'Invalid email or password'));
+        }
+
+        const isValidPassword = bcrypt.compare(password, validUser.password);
+        
+        if(!isValidPassword) {
+            return next(errorHandler(401, 'Wrong credentials'));
+        }
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        
+        const {password: pass, ...rest} = validUser._doc;
+
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json({ 
+            success: true,
+            message: 'logged in successfully',
+            rest,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signout = async (req, res, next) => {
+    try {
+      res.clearCookie("access_token")
+  
+      res.status(200).json({
+        success: true,
+        message: "User logged out successfully",
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
